4e8b5d9f8e4f3ec96d48d7c0fa9efcaed12784a9
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-11-20 22:54:16 +0100

    started working on docker backend

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "deep freezeeeeeee"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
