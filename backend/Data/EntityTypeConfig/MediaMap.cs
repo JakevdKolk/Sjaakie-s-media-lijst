1de49067ca07089eaa196315834d7f5bf7e5a77e
Jake <vandekolkjake@gmail.com>
2025-12-03 12:48:04 +0100

    added mediamap

--- backend/Data/EntityTypeConfig/MediaMap.cs
+++ backend/Data/EntityTypeConfig/MediaMap.cs
@@ -21,9 +21,21 @@ namespace backend.Data.EntityTypeConfig
                 .WithMany(mt => mt.Media)
                 .HasForeignKey(m => m.TypeId)
                 .OnDelete(DeleteBehavior.Cascade);
-            builder.HasMany(u => u.Spinoffs)
-                .WithOne(f => f.MainMedia)
-                .HasForeignKey(f => f.MainMediaId)
+            builder.HasMany(m => m.Spinoffs)
+                .WithOne(mm => mm.MainMedia)
+                .HasForeignKey(mm => mm.MainMediaId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(m => m.SpinoffOf)
+                .WithOne(mm => mm.SpinoffMedia)
+                .HasForeignKey(mm => mm.SpinoffMediaId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(m => m.MediaCharacters)
+                .WithOne(mc => mc.Media)
+                .HasForeignKey(mc => mc.MediaId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(m => m.UserLikes)
+                .WithOne(ulm => ulm.Media)
+                .HasForeignKey(ulm => ulm.MediaId)
                 .OnDelete(DeleteBehavior.Cascade);
 
         }
