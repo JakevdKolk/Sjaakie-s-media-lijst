75dcdc0ada602a518aff4cf830d6ad7d9928f857
Jake <vandekolkjake@gmail.com>
2025-12-16 16:12:59 +0100

    fixed docker compose with swagger conifg

--- Media_Backend/Controllers/BaseController.cs
+++ Media_Backend/Controllers/BaseController.cs
@@ -1,20 +1,22 @@
 ﻿using AutoMapper;
+using backend.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Media_Backend.Controllers
 {
-    public class BaseController
+    public class BaseController : Controller
     {
 
        protected readonly ILogger _logger;
        protected readonly IMapper _mapper;
+       protected readonly MediaListDbContext _dbContext;
 
-        public BaseController(ILogger logger, IMapper mapper)
+        public BaseController(ILogger logger, IMapper mapper, MediaListDbContext dbcontext)
         {
               _logger = logger;
               _mapper = mapper;
+              _dbContext = dbcontext;
         }
-
-
 
     }
 }
---
+++ Media_Backend/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Media_Backend.Controllers{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMapper mapper, MediaListDbContext dbcontext) : base(logger, mapper, dbcontext)
+        {
+        }
+
+     
+    }
+}
